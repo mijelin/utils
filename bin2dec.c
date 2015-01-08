0b76a5132cae1b9d38dd23c9c65f477158d7348f
Michelle Lin <mj.lin@berkeley.edu>
2015-01-07 21:08:30 -0800

    Initial commit

---
+++ bin2dec.c
@@ -0,0 +1,10 @@
+/** Binary to decimal converter. */
+
+#include <stdio.h>
+
+int
+main (int argc, char* argv[])
+{
+  /* Algorithm: repeatedly take the int mod 2, take remainders in reverse order
+   * to get bitstring. */
+}
